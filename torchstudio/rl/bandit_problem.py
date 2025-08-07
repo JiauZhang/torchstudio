88696011d582a835ebd59cd8f9efdcb0d02cf7ff
JiauZhang <1743960454@qq.com>
2025-08-07 23:38:33 +0800

    update

--- torchstudio/rl/bandit_problem.py
+++ torchstudio/rl/bandit_problem.py
@@ -19,10 +19,11 @@ class Bandit:
         mean = self.mean_action_value[action]
         return np.random.randn(1) + mean
 
-    def update_q(self, action, reward):
+    def update_q(self, action, reward, lr=None):
         q_reward = self.q_action_value[action]
         self.action_count[action] += 1
-        q_reward = q_reward + (reward - q_reward) / self.action_count[action]
+        step_size = lr if lr else 1 / self.action_count[action]
+        q_reward = q_reward + step_size * (reward - q_reward)
         self.q_action_value[action] = q_reward[0]
 
     def greedy_policy(self):
@@ -37,8 +38,8 @@ class Bandit:
     def random_policy(self):
         return random.choice(range(len(self.q_action_value)))
 
-    def train(self, steps, epsilon=0):
-        self.q_action_value = np.zeros_like(self.mean_action_value)
+    def greedy_train(self, steps, epsilon=0, init_value=0, lr=None):
+        self.q_action_value = np.ones_like(self.mean_action_value) * init_value
         self.action_count = np.zeros_like(self.mean_action_value)
         mean_reward = np.zeros((steps + 1, 1))
         mean_optimal_action = np.zeros(steps + 1)
@@ -55,7 +56,7 @@ class Bandit:
 
             reward = self.reward(action)
             mean_reward[n] = mean_reward[n - 1] + (reward - mean_reward[n - 1]) / n
-            self.update_q(action, reward)
+            self.update_q(action, reward, lr=lr)
 
         return np.array(mean_reward), mean_optimal_action
 
@@ -67,7 +68,7 @@ class Bandit:
         fig, axes = plt.subplots(1, 2, figsize=(12, 5))
         for eps, c in zip(epsilon, color):
             for _ in tqdm(range(runs)):
-                reward, optimal_action = self.train(steps, epsilon=eps)
+                reward, optimal_action = self.greedy_train(steps, epsilon=eps)
                 mean_reward.append(reward)
                 mean_optimal_action.append(optimal_action)
             mean_reward = np.array(mean_reward).mean(axis=0)
@@ -86,7 +87,83 @@ class Bandit:
         plt.savefig('./figure_2_2.png')
         plt.close()
 
+    def figure_2_3(self, runs=500, steps=1000):
+        mean_optimal_action = []
+        epsilon = [0.0, 0.0, 0.1]
+        # -2 < all true mean value
+        init_value = [-2, 5, 0]
+        color = ['g', 'r', 'b']
+        _, axes = plt.subplots(1, 1, figsize=(12, 5))
+        for eps, c, value in zip(epsilon, color, init_value):
+            for _ in tqdm(range(runs)):
+                _, optimal_action = self.greedy_train(steps, epsilon=eps, init_value=value, lr=0.1)
+                mean_optimal_action.append(optimal_action)
+            mean_optimal_action = np.array(mean_optimal_action).mean(axis=0)
+            axes.plot(mean_optimal_action, color=c, label=f'$Q_1 = {value}, \\epsilon = {eps:.02f}$')
+            mean_optimal_action = []
+
+        axes.set_xlabel('steps')
+        axes.set_ylabel('optimal action')
+        axes.legend()
+        plt.savefig('./figure_2_3.png')
+        plt.close()
+
+    def ucb_policy(self, c, t):
+        action = 0
+        max_action_value = self.q_action_value[0]
+        for i in range(len(self.q_action_value)):
+            if self.action_count[i] == 0:
+                action = i
+                break
+            else:
+                action_value = self.q_action_value[i] + c * np.sqrt(np.log(t) / self.action_count[i])
+                if action_value > max_action_value:
+                    action = i
+                    max_action_value = action_value
+        return action
+
+    def ucb_train(self, steps, c, init_value=0, lr=None):
+        self.q_action_value = np.ones_like(self.mean_action_value) * init_value
+        self.action_count = np.zeros_like(self.mean_action_value)
+        mean_reward = np.zeros((steps + 1, 1))
+
+        for n in range(1, steps+1):
+            action = self.ucb_policy(c, n)
+            reward = self.reward(action)
+            mean_reward[n] = mean_reward[n - 1] + (reward - mean_reward[n - 1]) / n
+            self.update_q(action, reward, lr=lr)
+
+        return np.array(mean_reward)
+
+    def figure_2_4(self, runs=500, steps=1000):
+        mean_reward = []
+        train_fn = [self.greedy_train, self.ucb_train, self.ucb_train]
+        train_kwargs = [{'epsilon': 0.1}, {'c': 2}, {'c': 1}]
+        label_template = [
+            '$\\epsilon$-greedy $\\epsilon = {epsilon}$',
+            'UCB $c = {c}$',
+            'UCB $c = {c}$',
+        ]
+        color = ['g', 'r', 'b']
+        _, axes = plt.subplots(1, 1, figsize=(12, 5))
+        for kwargs, c, train, label in zip(train_kwargs, color, train_fn, label_template):
+            for _ in tqdm(range(runs)):
+                reward = train(steps, **kwargs)
+                if isinstance(reward, tuple): reward = reward[0]
+                mean_reward.append(reward)
+            mean_reward = np.array(mean_reward).mean(axis=0)
+            axes.plot(mean_reward, color=c, label=label.format(**kwargs))
+            mean_reward = []
+
+        axes.set_xlabel('steps')
+        axes.set_ylabel('average reward')
+        axes.legend()
+        plt.savefig('./figure_2_4.png')
+        plt.close()
+
 if __name__ == '__main__':
     bandit = Bandit()
     bandit.figure_2_1()
     bandit.figure_2_2()
+    bandit.figure_2_3()
+    bandit.figure_2_4()
