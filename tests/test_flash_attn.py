4d38355a6cd5d8eb0463d0e7ee9fb1c4faf90da5
JiauZhang <1743960454@qq.com>
2025-10-05 22:21:40 +0800

    flash attention

---
+++ tests/test_flash_attn.py
@@ -0,0 +1,32 @@
+import pytest
+import numpy as np
+from torchstudio.flash_attn import (
+    softmax,
+    full_attention,
+    flash_attention,
+)
+
+@pytest.mark.parametrize('shape', [(32, 16), (64, 32), (127, 53)])
+def test_full_attention(shape):
+    q = np.random.randn(*shape)
+    k = np.random.randn(*shape)
+    v = np.random.randn(*shape)
+    score = full_attention(q, k, v)
+    gt = softmax(q @ k.T, axis=1) @ v
+    assert score.shape == gt.shape
+    assert (np.abs(gt - score) < 1e-6).all()
+
+@pytest.mark.parametrize(
+    'shape, M', [
+        ((32, 16), 256), ((64, 32), 233), ((127, 53), 677),
+        ((32, 64), 128), ((64, 128), 1024), ((127, 256), 512),
+    ]
+)
+def test_flash_attention(shape, M):
+    q = np.random.randn(*shape)
+    k = np.random.randn(*shape)
+    v = np.random.randn(*shape)
+    score = flash_attention(q, k, v, M)
+    gt = softmax(q @ k.T, axis=1) @ v
+    assert score.shape == gt.shape
+    assert (np.abs(gt - score) < 1e-6).all()
---
+++ torchstudio/flash_attn/__init__.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def softmax(x, axis):
+    x_shifted = x - np.max(x, axis=axis, keepdims=True)
+    exp_x = np.exp(x_shifted)
+    return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
+
+def tiled_attention(q, k, v, output, m_base, norm_base):
+    # q shape: [B_r, d], k shape: [B_c, d]
+    s = q @ k.T
+    m = np.max(s, axis=1, keepdims=True)
+    m_m_base = np.concat((m, m_base), axis=1)
+    m_new = np.max(m_m_base, axis=1, keepdims=True)
+    p = np.exp(s - m)
+    norm = np.sum(p, axis=1, keepdims=True)
+    exp_m_m_new = np.exp(m - m_new)
+    exp_m_base_m_new = np.exp(m_base - m_new)
+    norm_new = exp_m_base_m_new * norm_base + exp_m_m_new * norm
+    output =  (norm_base * exp_m_base_m_new * output + exp_m_m_new * p @ v) / norm_new
+    return output, m_new, norm_new
+
+def full_attention(q, k, v):
+    output = np.zeros_like(q)
+    norm_base = np.zeros((q.shape[0], 1))
+    m_base = norm_base - np.inf
+    output = tiled_attention(q, k, v, output, m_base, norm_base)[0]
+    return output
+
+def __fetch_block(data, top, left, height, width):
+    bottom = min(top + height, data.shape[0])
+    right = min(left + width, data.shape[1])
+    return data[top:bottom, left:right]
+
+def flash_attention(q, k, v, M):
+    N, d = q.shape
+    B_c = int(np.ceil(M / 4 / d))
+    B_r = int(min(np.ceil(M / 4 / d).item(), d))
+    T_r = int(np.ceil(N / B_r))
+    T_c = int(np.ceil(N / B_c))
+
+    output = np.zeros_like(q)
+    norm_base = np.zeros((q.shape[0], 1))
+    m_base = norm_base - np.inf
+
+    for j in range(T_c):
+        # [B_c, d]
+        K_j = __fetch_block(k, j * B_c, 0, B_c, d)
+        V_j = __fetch_block(v, j * B_c, 0, B_c, d)
+        for i in range(T_r):
+            # [B_r, d]
+            Q_i = __fetch_block(q, i * B_r, 0, B_r, d)
+            O_i = __fetch_block(output, i * B_r, 0, B_r, d)
+            # [B_r, 1]
+            norm_i = __fetch_block(norm_base, i * B_r, 0, B_r, 1)
+            m_i = __fetch_block(m_base, i * B_r, 0, B_r, 1)
+            O_i[...], m_i[...], norm_i[...] = tiled_attention(Q_i, K_j, V_j, O_i, m_i, norm_i)
+
+    return output
